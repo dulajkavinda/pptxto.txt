5934ae4ca9fb53b3715fc313a49dcc8c2c4e9bd3
Dulaj Kavinda <dulajkaavinda@gmail.com>
2021-01-15 09:52:46 +0530

    removed unwanted files

--- index.js
+++
@@ -1,7 +0,0 @@
-var textract = require("./lib");
-textract.fromUrl(
-  "https://scholar.harvard.edu/files/torman_personal/files/samplepptx.pptx",
-  function (error, text) {
-    console.log(text[1]);
-  }
-);
